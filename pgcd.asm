484882d39086d25dbf04198c91e545d95be712ad
BaubYo <alexis.lombardi@etu.univ-smb.fr>
2022-04-08 09:38:50 +0200

    Code commenté ! :+1:

--- pgcd.asm
+++ pgcd.asm
@@ -4,38 +4,38 @@ DATA SEGMENT
 	aux DD
 DATA ENDS
 CODE SEGMENT
-	 in eax
-	 mov a, eax
-	 in eax
-	 mov b, eax
+	in eax
+	mov a, eax
+	in eax
+	mov b, eax
 debut_while_1:
-	 mov eax, 0
-	 push eax
-	 mov eax, b
-	 pop ebx
-	 sub eax,ebx
-	 jle faux_gt_1
-	 mov eax,1
-	 jmp sortie_gt_1
+	mov eax, 0
+	push eax
+	mov eax, b
+	pop ebx
+	sub eax,ebx
+	jle faux_gt_1
+	mov eax,1
+	jmp sortie_gt_1
 faux_gt_1:
-	 mov eax,0
+	mov eax,0
 sortie_gt_1:
-	 jz sortie_while_1
-	 mov eax, b
-	 push eax
-	 mov eax, a
-	 pop ebx
-	 mov ecx,eax
-	 div ecx,ebx
-	 mul ecx,ebx
-	 sub eax,ecx
-	 mov aux, eax
-	 mov eax, b
-	 mov a, eax
-	 mov eax, aux
-	 mov b, eax
-	 jmp debut_while_1
+	jz sortie_while_1
+	mov eax, b
+	push eax
+	mov eax, a
+	pop ebx
+	mov ecx,eax
+	div ecx,ebx
+	mul ecx,ebx
+	sub eax,ecx
+	mov aux, eax
+	mov eax, b
+	mov a, eax
+	mov eax, aux
+	mov b, eax
+	jmp debut_while_1
 sortie_while_1:
-	 mov eax,a
-	 out eax
+	mov eax,a
+	out eax
 CODE ENDS
--- src/main/java/fr/usmb/m1isc/compilation/tp/Arbre.java
+++ src/main/java/fr/usmb/m1isc/compilation/tp/Arbre.java
@@ -11,18 +11,20 @@ import java.util.Optional;
 import java.util.Stack;
 
 public class Arbre {
-	//var globale
+	//Variable globale
 	public static String stack = "ebx";
+    //Enmération
+	public static enum Type {SEMI,LET,WHILE,INPUT,OUTPUT,INF,PLUS,MOINS,DIV,MUL,MOD,ENTIER,VIDE,EQ,IF,THEN,NOT,AND,OR,INFEQ,MINUS,NULL,IDENT};
+	//Opération possible
 	public String[] op ={"/","*","+","-"};
 	
-	public static enum Type {SEMI,LET,WHILE,INPUT,OUTPUT,INF,PLUS,MOINS,DIV,MUL,MOD,ENTIER,VIDE,EQ,IF,THEN,NOT,AND,OR,INFEQ,MINUS,NULL,IDENT};
+    //Attributs
 	private Type type;
-	
-	
 	private String racine;
 	private Arbre arbreG;
 	private Arbre arbreD;
 	
+    //Getter et Setters
 	public Type getType() {
 		return type;
 	}
@@ -48,6 +50,7 @@ public class Arbre {
 		this.arbreD = arbreD;
 	}
 	
+    //Constructeur
 	public Arbre(String racine, Arbre arbreG, Arbre arbreD,Type type) {
 		super();
 		this.racine = racine;
@@ -55,6 +58,9 @@ public class Arbre {
 		this.arbreD = arbreD;
 		this.type=type;
 	}
+	
+    //On réécrit toString pour afficher le type Arbre
+    // cela dépend du contenu des fils et de la racine
 	@Override
 	public String toString() {
 		if(getArbreG()==null && getArbreD()==null) {
@@ -70,123 +76,152 @@ public class Arbre {
 			return "("+racine +" "+getArbreG().toString()+" "+getArbreD().toString() +")";
 		}
 		return "";
-		
-		
 	}
 	
+    //Permet de convertir une "séquence" (dans notre cup) vers un language en Assembly
+    //On se servs d'une HashMap "vars" pour connaitre le nom des variables
 	public String toAssembly(List<String> vars) {
+        
+        //On définis les variables
 		String text = "DATA SEGMENT\n";
-		vars = removeDoublon(vars);
+		vars = removeDoublon(vars);        
 		for (String elem : vars) {
 			text+="\t" + elem + " DD\n";
 		}
 		text += "DATA ENDS\n" + "CODE SEGMENT\n";
+
+        //On convertie le code
 		text+=convertArbre(this,true, vars,false);
-		//convertArbre(this,true, vars);
         
         text += "CODE ENDS";
         return text;
     }
 	
+    //Permet d'enlever les doublons dans les variables
 	private List<String> removeDoublon(List<String> vars) {
+        //Création d'un tableau vide
 		List<String> l = new ArrayList<String>();
 		boolean existe;
+        //On parcours les variables de vars
 		for (String s : vars) {
 			existe=false;
+            //On parcours les variables de l pour vérifier que la variable courante n'est pas déjà présente
 			for (String s2 : l) {
 				if(s2.equals(s)) {
 					existe=true;
 					break;
 				}
 			}
+            //Et on l'ajoute a l si elle n'est pas présente
 			if(!existe) {
 				l.add(s);
 			}
 		}
 		return l;
 	}
-	private String convertArbre(Arbre arbre, boolean sens,List<String> vars,boolean lastOp) {
+
+    //! nettoyer le sens
+
+    //Permet de convertir un arbre en code Assembly
+    //Cette fonction va se rappeler récursivement
+    //Elle a besoin d'un arbre, d'une HashMap, et d'une variable pour savoir si il y a une opération qui a eu lieu
+	private String convertArbre(Arbre arbre, boolean sens, List<String> vars, boolean lastOp) {
 		String text = "";
 		
+        //On trouve le type de l'abre
 		switch (arbre.type) {
+
+            //Si c'est un ";"
 			case SEMI:
 				text += convertArbre(arbre.getArbreG(), !sens,vars,lastOp);
-				if(arbre.getArbreD().racine==";")
+				if(arbre.getArbreD().racine == ";")
 					text += convertArbre(arbre.getArbreD(), !sens,vars,lastOp);
 				else {
 					text += convertArbre(arbre.getArbreD(), !sens,vars,true);
-				}
-						
+				}	
 				break;
 				
+            //Si c'est un "let"
 			case LET:
 				text += convertArbreLetPartieDroite(arbre.getArbreD(), !sens,vars,1,lastOp) + convertArbre(arbre.getArbreG(), !sens,vars,lastOp);
 				if(!lastOp && !arbre.getArbreD().racine.equals("INPUT")) {
-					text += "\t mov eax, "+ arbre.getArbreG().racine + "\n";
-					text += "\t push eax\n";
+					text += "\tmov eax, "+ arbre.getArbreG().racine + "\n";
+					text += "\tpush eax\n";
 				}
-				
 				break;
 				
+            //Si c'est une "multiplication"
 			case MUL:
 				if(Arrays.stream(op).anyMatch(arbre.getArbreG().racine::equals)) {
 					text+=convertArbre(arbre.getArbreG(), !sens,vars,lastOp);
 					text+=convertArbre(arbre.getArbreD(), !sens,vars,lastOp);
-					text+="\t pop ebx\n";
-					text+="\t mul ebx, eax\n";
-					text+="\t mov eax, ebx\n";
-					text+="\t push eax\n";
+					text+="\tpop ebx\n";
+					text+="\tmul ebx, eax\n";
+					text+="\tmov eax, ebx\n";
+					text+="\tpush eax\n";
 				}
 				else {
 					text+=convertArbre(arbre.getArbreD(), !sens,vars,lastOp);
 					text+=convertArbre(arbre.getArbreG(), !sens,vars,lastOp);
-					text+="\t mul eax, ebx\n";
-					text+="\t push eax\n";
+					text+="\tmul eax, ebx\n";
+					text+="\tpush eax\n";
 				}
 				break;
+
+            //Si c'est une division
 			case DIV:
 				if(Arrays.stream(op).anyMatch(arbre.getArbreG().racine::equals)) {
 					text+=convertArbre(arbre.getArbreG(), !sens,vars,lastOp);
 					text+=convertArbre(arbre.getArbreD(), !sens,vars,lastOp);
-					text+="\t pop ebx\n";
-					text+="\t div ebx, eax\n";
-					text+="\t mov eax, ebx\n";
+					text+="\tpop ebx\n";
+					text+="\tdiv ebx, eax\n";
+					text+="\tmov eax, ebx\n";
 					
-					text+="\t push eax\n";
+					text+="\tpush eax\n";
 				}
 				else {
 					text+=convertArbre(arbre.getArbreD(), !sens,vars,lastOp);
 					text+=convertArbre(arbre.getArbreG(), !sens,vars,lastOp);
-					text+="\t div eax, ebx\n";
-					text+="\t push eax\n";
+					text+="\tdiv eax, ebx\n";
+					text+="\tpush eax\n";
 				}
 				break;
+
+            //Si c'est un "while"
 			case WHILE:
 				text+="debut_while_1:\n";
 				text+= convertArbreCond(arbre.getArbreG(),vars);
 				text+="faux_gt_1:\n";
-				text+="\t mov eax,0\n";
+				text+="\tmov eax,0\n";
 				text+="sortie_gt_1:\n";
-				text+="\t jz sortie_while_1\n";
+				text+="\tjz sortie_while_1\n";
 				text+= convertArbreAction(arbre.getArbreD(),vars);
-				text+="\t jmp debut_while_1\n";
+				text+="\tjmp debut_while_1\n";
 				text+="sortie_while_1:\n";
 				break;
 				
+            //Si c'est une "input"
 			case INPUT:
-				text+="\t in eax\n";
+				text+="\tin eax\n";
 				break;
+
+            //Si c'est une "output"
 			case OUTPUT:
-				text+="\t mov eax,"+ arbre.getArbreG().racine+"\n";
-				text+="\t out eax\n";
+				text+="\tmov eax,"+ arbre.getArbreG().racine+"\n";
+				text+="\tout eax\n";
 				break;
+            
+            //Si c'est un entier
 			case ENTIER:
-				text+="\t mov eax, " + arbre.racine +"\n";
+				text+="\tmov eax, " + arbre.racine +"\n";
 				break;
+            
+            //Si c'est une variable
 			case IDENT:
-				text+="\t mov "+arbre.racine+", eax\n";
+				text+="\tmov "+arbre.racine+", eax\n";
 				break;
 	
+            //Sinon c'est une erreur
 			default:
 				text += "Error";
 				break;
@@ -194,105 +229,82 @@ public class Arbre {
 		return text;
 	}
 
+    //Fonction récursive qui est utilisé pour le "while"
 	private String convertArbreAction(Arbre arbre,List<String> vars) {
 		String text = "";
 		
 		switch (arbre.type) {
+            //Basique conversion a gauche et a droite pour faire tout l'arbre avec les ;
 			case SEMI:
 				text += convertArbreAction(arbre.getArbreG(),vars);
 				text += convertArbreAction(arbre.getArbreD(),vars);
-						
 				break;
+
+            //Gere les let avec des variables globables donc pas de probleme de pop de stack
 			case LET:
 				text += convertArbreAction(arbre.getArbreD(),vars);
-				text += "\t mov "+arbre.getArbreG().racine+", eax\n";
-
+				text += "\tmov "+arbre.getArbreG().racine+", eax\n";
 				break;
+
+            //Modulo
 			case MOD:
 				text += convertArbreAction(arbre.getArbreD(),vars);
-				text += "\t push eax\n";
+				text += "\tpush eax\n";
 				text += convertArbreAction(arbre.getArbreG(),vars);
-				text += "\t pop ebx\n";
-				text += "\t mov ecx,eax\n";
-				text += "\t div ecx,ebx\n";
-				text += "\t mul ecx,ebx\n";
-				text += "\t sub eax,ecx\n";
+				text += "\tpop ebx\n";
+				text += "\tmov ecx,eax\n";
+				text += "\tdiv ecx,ebx\n";
+				text += "\tmul ecx,ebx\n";
+				text += "\tsub eax,ecx\n";
 				break;
 				
-				
-			case MUL:
-				if(Arrays.stream(op).anyMatch(arbre.getArbreG().racine::equals)) {
-					text+=convertArbreAction(arbre.getArbreG(),vars);
-					text+=convertArbreAction(arbre.getArbreD(),vars);
-					text+="\t pop ebx\n";
-					text+="\t mul ebx, eax\n";
-					text+="\t mov eax, ebx\n";
-				}
-				else {
-					text+=convertArbreAction(arbre.getArbreD(),vars);
-					text+=convertArbreAction(arbre.getArbreG(),vars);
-					text+="\t mul eax, ebx\n";
-				}
-
-				break;
-			case DIV:
-				if(Arrays.stream(op).anyMatch(arbre.getArbreG().racine::equals)) {
-					text+=convertArbreAction(arbre.getArbreG(),vars);
-					text+=convertArbreAction(arbre.getArbreD(),vars);
-					text+="\t pop ebx\n";
-					text+="\t div ebx, eax\n";
-					text+="\t mov eax, ebx\n";
-				}
-				else {
-					text+=convertArbreAction(arbre.getArbreD(),vars);
-					text+=convertArbreAction(arbre.getArbreG(),vars);
-					text+="\t div eax, ebx\n";
-					
-				}
-				break;
+			//Cas parentheses
 			case VIDE:
 				text+=convertArbreAction(arbre.getArbreG(),vars);
 				break;
-				
+
+			//Copie entier dans eax
 			case ENTIER:
-				text+="\t mov eax, " + arbre.racine +"\n";
+				text+="\tmov eax, " + arbre.racine +"\n";
 				break;
-				
+			//Copie var globable dans eax
 			case IDENT:
-				text+="\t mov eax, "+ arbre.racine+"\n";
+				text+="\tmov eax, "+ arbre.racine+"\n";
 				break;
 			default:
 				text += "Error";
-					
 				break;
 		}
 		return text;
 	}
+    //Gere la partie condition de l'arbre
 	private String convertArbreCond(Arbre arbre, List<String> vars) {
-		// TODO Auto-generated method stub
 		String text = "";
 		
 		switch (arbre.type) {
-			
+			//Gere le inf 
 			case INF:
 				text+=convertArbreCond(arbre.getArbreG(),vars);
-				text+="\t push eax\n";
+				text+="\tpush eax\n";
 				text+=convertArbreCond(arbre.getArbreD(),vars);
-				text+="\t pop ebx\n";
-				text+="\t sub eax,ebx\n";
-				text+="\t jle faux_gt_1\n";
-				text+="\t mov eax,1\n";
-				text+="\t jmp sortie_gt_1\n";
+				text+="\tpop ebx\n";
+				text+="\tsub eax,ebx\n";
+				text+="\tjle faux_gt_1\n";
+				text+="\tmov eax,1\n";
+				text+="\tjmp sortie_gt_1\n";
 				
 				break;
+            //Ignore la valeur de la parenthese
 			case VIDE:
 				text+=convertArbreCond(arbre.getArbreG(),vars);
 				break;
+            //Copie entier dans eax
 			case ENTIER:
-				text+="\t mov eax, " + arbre.racine +"\n";
+				text+="\tmov eax, " + arbre.racine +"\n";
 				break;
+            //Copie var globable dans eax
 			case IDENT:
-				text+="\t mov eax, "+ arbre.racine +"\n";
+				text+="\tmov eax, "+ arbre.racine +"\n";
 				break;
 			default:
 				text += "Error";
@@ -304,10 +316,13 @@ public class Arbre {
 		
 	}
 	//PARTIE DROITE
+    //Gere la partie droite des let qui possede potentiellement des variables et donc faut pop la stack pour les recuperer
+    //C'est quasiment la meme fonction que convertArbre mais elle a un parametre en plus permettant de savoir si on doit faire certain push en fonction si on est a la fin de la methode et a la bonne profondeur
 	private String convertArbreLetPartieDroite(Arbre arbre, boolean sens,List<String> vars,int profondeur,boolean lastOp) {
 		String text = "";
 		
 		switch (arbre.type) {
+            //
 			case SEMI:
 				text += convertArbreLetPartieDroite(arbre.getArbreG(), !sens,vars,profondeur+1,lastOp);
 				if(arbre.getArbreD().racine==";")
@@ -317,61 +332,63 @@ public class Arbre {
 				}
 						
 				break;
-				
+			
+            //
 			case LET:
 				text += convertArbreLetPartieDroite(arbre.getArbreD(), !sens,vars,profondeur+1,lastOp) + convertArbreLetPartieDroite(arbre.getArbreG(), !sens,vars,profondeur+1,lastOp);
 				if(!lastOp) {
-					text += "\t mov eax, "+ arbre.getArbreG().racine + "\n";
-					text += "\t push eax\n";
+					text += "\tmov eax, "+ arbre.getArbreG().racine + "\n";
+					text += "\tpush eax\n";
 				}
 				break;
-				
+			//En fonction de si le prochain arbre a une operation cela changera le sens de l'operation et mov/pop ou non
 			case MUL:
 				if(Arrays.stream(op).anyMatch(arbre.getArbreG().racine::equals)) {
 					text+=convertArbreLetPartieDroite(arbre.getArbreG(), !sens,vars,profondeur+1,lastOp);
 					text+=convertArbreLetPartieDroite(arbre.getArbreD(), !sens,vars,profondeur+1,lastOp);
-					text+="\t pop ebx\n";
-					text+="\t mul ebx, eax\n";
-					text+="\t mov eax, ebx\n";
+					text+="\tpop ebx\n";
+					text+="\tmul ebx, eax\n";
+					text+="\tmov eax, ebx\n";
 				}
 				else {
 					text+=convertArbreLetPartieDroite(arbre.getArbreD(), !sens,vars,profondeur+1,lastOp);
 					text+=convertArbreLetPartieDroite(arbre.getArbreG(), !sens,vars,profondeur+1,lastOp);
-					text+="\t mul eax, ebx\n";
+					text+="\tmul eax, ebx\n";
 				}
 				
 				if(profondeur!=1 || !lastOp) {
-					text+="\t push eax\n";
+					text+="\tpush eax\n";
 				}
 				break;
 			case DIV:
 				if(Arrays.stream(op).anyMatch(arbre.getArbreG().racine::equals)) {
 					text+=convertArbreLetPartieDroite(arbre.getArbreG(), !sens,vars,profondeur+1,lastOp);
 					text+=convertArbreLetPartieDroite(arbre.getArbreD(), !sens,vars,profondeur+1,lastOp);
-					text+="\t pop ebx\n";
-					text+="\t div ebx, eax\n";
-					text+="\t mov eax, ebx\n";
+					text+="\tpop ebx\n";
+					text+="\tdiv ebx, eax\n";
+					text+="\tmov eax, ebx\n";
 				}
 				else {
 					text+=convertArbreLetPartieDroite(arbre.getArbreD(), !sens,vars,profondeur+1,lastOp);
 					text+=convertArbreLetPartieDroite(arbre.getArbreG(), !sens,vars,profondeur+1,lastOp);
-					text+="\t div eax, ebx\n";
+					text+="\tdiv eax, ebx\n";
 					
 				}
 				if(profondeur!=1 || !lastOp) {
-					text+="\t push eax\n";
+					text+="\tpush eax\n";
 				}
 				break;
+            //Simple gestion de l'input
 			case INPUT:
-				text+="\t in eax\n";
+				text+="\tin eax\n";
 				break;
-				
+			//Copie la valeur de l'entier dans eax
 			case ENTIER:
-				text+="\t mov eax, " + arbre.racine +"\n";
+				text+="\tmov eax, " + arbre.racine +"\n";
 				break;
-				
+			//Vu qu'on est a droite du let alors on pop de la stack la variable
 			case IDENT:
-				text+="\t pop ebx\n";
+				text+="\tpop ebx\n";
 				break;
 			default:
 				text += "Error";
@@ -380,25 +397,27 @@ public class Arbre {
 		return text;
 	}
 	
+    //Permet de créer le fichier pgcd.asm,
+    // et de lui mettre le code Assembly
 	static public void makeFile(String str) {
 		try {
 		      File myObj = new File("pgcd.asm");
 		      if (myObj.createNewFile()) {
-		        System.out.println("File created: " + myObj.getName());
+		        System.out.println("Fichier créer: " + myObj.getName());
 		      } else {
-		        System.out.println("File already exists.");
+		        System.out.println("Le fichier existe déjà");
 		      }
 		    } catch (IOException e) {
-		      System.out.println("An error occurred.");
+		      System.out.println("Il y a une erreur dans la création du fichier");
 		      e.printStackTrace();
 		    }
 		try {
 	      FileWriter myWriter = new FileWriter("pgcd.asm");
 	      myWriter.write(str);
 	      myWriter.close();
-	      System.out.println("Successfully wrote to the file.");
+	      System.out.println("Ecriture dans le fichier réussi");
 	    } catch (IOException e) {
-	      System.out.println("An error occurred.");
+	      System.out.println("Il y a une erreur dans l'écriture du fichier");
 	      e.printStackTrace();
 	    }
 	}
